594fc3aa2e807d7a47a81780a175cc429b74d835
Kesh Laarbi <kesh2072@gmail.com>
2025-10-07 16:00:47 +0100

    added ability to save books

--- backend/articlescout/urls.py
+++ backend/articlescout/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("books.urls"))
+    path("books/", include("books.urls")),
+    path("", RedirectView.as_view(url="/books/", permanent=False)),
 ]
--- backend/books/urls.py
+++ backend/books/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("api/book/", views.view_book_api, name="view_book_api"),
+    path('save/', views.save_article, name='save_article'),
+    path('saved/', views.list_saved_articles, name='list_saved_articles'),
 ]
