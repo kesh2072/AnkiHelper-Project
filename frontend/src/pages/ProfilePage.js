4a76fa27c771557a7a671434b2a1d7213ef4e5a1
Kesh Laarbi <kesh2072@gmail.com>
2025-10-22 13:35:41 +0100

    fixed routing for creating an account

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -1,22 +1,30 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function ProfilePage() {
   const { logout } = useContext(AuthContext);
+  const {isLoggedIn} = useContext(AuthContext);
   const navigate = useNavigate();
 
   const handleLogout = () => {
     logout();
-    navigate("/login");
   };
 
   return (
     <div className="container py-5">
       <h1>Profile Page</h1>
+      {isLoggedIn ? (
       <button onClick={handleLogout} className="btn btn-danger">
         Logout
       </button>
+      ) : (
+        <div>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Create an account</Link>
+      </div>
+      )}
     </div>
   );
 }
