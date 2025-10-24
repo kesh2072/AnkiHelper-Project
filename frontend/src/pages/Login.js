244a80172aa0e40e7a10ac142f3780f68af2941d
Kesh Laarbi <kesh2072@gmail.com>
2025-10-24 12:00:44 +0100

    tailwind still not working, will come back to it

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -24,24 +24,75 @@ function Login() {
     }
   };
 
-  return (
-    <div className="container py-5">
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Log In</button>
-      </form>
+return (
+    <div className="flex min-h-screen flex-col justify-center px-6 py-12 lg:px-8 bg-white">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl font-bold tracking-tight text-gray-900">
+          Sign in to your account
+        </h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form
+          onSubmit={handleSubmit}
+          method="POST"
+          className="space-y-6"
+        >
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-900"
+            >
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                type="email"
+                name="email"
+                required
+                autoComplete="email"
+                className="block w-full rounded-md border border-gray-300 bg-white px-3 py-1.5
+                text-base text-gray-900 placeholder:text-gray-400 focus:border-indigo-600
+                focus:ring-2 focus:ring-indigo-600 sm:text-sm"
+              />
+            </div>
+          </div>
+
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-900"
+            >
+              Password
+            </label>
+            <div className="mt-2">
+              <input
+                id="password"
+                type="password"
+                name="password"
+                required
+                autoComplete="current-password"
+                className="block w-full rounded-md border border-gray-300 bg-white px-3 py-1.5
+                text-base text-gray-900 placeholder:text-gray-400 focus:border-indigo-600
+                focus:ring-2 focus:ring-indigo-600 sm:text-sm"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5
+              text-sm font-semibold text-white shadow hover:bg-indigo-500
+              focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2
+              focus-visible:outline-indigo-600"
+            >
+              Sign in
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
