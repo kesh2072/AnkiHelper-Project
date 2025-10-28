7ec79a2cca32e30abf02d22aed35b0f79b93006d
Kesh Laarbi <kesh2072@gmail.com>
2025-10-28 18:13:19 +0000

    added ability to see all cards in a given Anki deck

--- backend/ankiConnect/urls.py
+++ backend/ankiConnect/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("api/anki/", views.view_decks, name="get_anki_decks"),
-    path("api/ankicard", views.view_cards, name="view cards")
+    path("api/ankicard", views.view_cards, name="view cards"),
+    path("api/deck/<str:deck_name>/", views.get_deck_cards),
 ]
--- backend/ankiConnect/views.py
+++ backend/ankiConnect/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .anki_utils import get_decks, get_card_info, open_anki, is_anki_open
 import os
+import requests
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
 
 def view_decks(request):
     open_anki()
@@ -20,3 +23,22 @@ def view_cards(request):
     data = get_card_info()
     print(data)
     return JsonResponse(data, safe=False)
+
+@api_view(['GET'])
+def get_deck_cards(request, deck_name):
+    find_res = requests.post("http://127.0.0.1:8765", json={
+        "action": "findCards",
+        "version": 6,
+        "params": {"query": f'deck:"{deck_name}"'}
+    }).json()
+
+    card_ids = find_res.get("result", [])
+
+    info_res = requests.post("http://127.0.0.1:8765", json={
+        "action": "cardsInfo",
+        "version": 6,
+        "params": {"cards": card_ids}
+    }).json()
+    print(info_res.get("result", []))
+
+    return Response(info_res.get("result", []))
