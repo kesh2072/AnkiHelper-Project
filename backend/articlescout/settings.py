dc074c71d845627c33da26c03bec4e3990bfa338
kesh2072 <kesh2072@gmail.com>
2025-10-29 12:38:20 +0000

    changed DB from sqlite to Postgresql

--- backend/articlescout/settings.py
+++ backend/articlescout/settings.py
@@ -98,8 +98,12 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'AnkiHelp_db',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
