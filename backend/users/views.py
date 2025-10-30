7093cb4645cc6d656f8dd3f229caf66a2b08c3ad
Kesh Laarbi <kesh2072@gmail.com>
2025-10-30 11:40:29 +0000

    fixed database migration to postgres

--- backend/users/views.py
+++ backend/users/views.py
@@ -7,13 +7,14 @@ from rest_framework import generics
 from .serializers import UserSerializer
 import json
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework import status
 
 class RegisterUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [AllowAny]
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
