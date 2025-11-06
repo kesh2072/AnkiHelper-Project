6e3b235eeba77ffd8a318806d3d9c77830a2cc1f
Kesh Laarbi <kesh2072@gmail.com>
2025-11-06 12:30:24 +0000

    more buttons

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -29,8 +29,12 @@ function ProfilePage() {
       </div>
       ) : (
         <div>
-          <Link to="/login" className="btn btn-primary rounded-pill px-3" type="button">Login</Link>
-          <Link to="/register" className="btn btn-primary rounded-pill px-3" type="button">Create an account</Link>
+          <a href="/login" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+            Login
+          </a>
+          <a href="/register" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+            Create an account
+          </a>
       </div>
       )}
     </div>
